3e2788807912114dec2c52b177fb3d44145596e8
Aditya Pathre <pathreaig@gmail.com>
2020-09-15 00:29:32 +0100

    added burger menu

--- src/Header.js
+++ src/Header.js
@@ -1,23 +1,40 @@
-import React from 'react'
+import React, {useState} from 'react'
 import PersonIcon from '@material-ui/icons/Person';
 import ForumIcon from '@material-ui/icons/Forum';
 import IconButton from '@material-ui/core/IconButton';
 import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
 import './Header.css';
 import { Link, useHistory } from 'react-router-dom';
-
+import {slide as Menu} from 'react-burger-menu';
 
 function Header({backButton}) {
     const history=useHistory();
+    const [menuOpen,setMenuOpen]=useState(false);
+    console.log('menuOpen is:',menuOpen);
+    const handleStateChange=(state)=>{
+        console.log('reached handleStateChange in header');
+        setMenuOpen(state.isOpen);
+    }
+    const closeMenu=()=>{
+        console.log('reached closeMenu');
+        setMenuOpen(false);
+    }
+    const toggleMenu=()=>setMenuOpen({menuOpen:!menuOpen})
+
     return (
         <div className="header">
             {(backButton)?
             (<IconButton onClick={()=>history.goBack()}>
             <ArrowBackIosIcon fontSize="large"/>
             </IconButton>)
-            :(<IconButton>
-            <PersonIcon fontSize="large"/>
-            </IconButton>)}
+            :(<Menu
+                  isOpen={menuOpen}
+                  onStateChange={(state)=>handleStateChange(state)}>
+                    <a onClick={()=>closeMenu()} href="/" >Home</a>
+                    <a onClick={()=>closeMenu()} href="/chat">Chat</a>
+                    <a onClick={()=>closeMenu()} >Contact</a>
+                </Menu>
+            )}
             <Link to="/">
             <img 
             className="tinder_logo"
